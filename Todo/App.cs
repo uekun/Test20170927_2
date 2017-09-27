d6cfa3986f49537b023feca3aeefc7b1d992e8e1
uekun <32317481+uekun@users.noreply.github.com>
2017-09-27 11:36:31 +0900

    initial commit

---
+++ Todo/App.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using System.Diagnostics;
+using Microsoft.Azure.Mobile;
+using Microsoft.Azure.Mobile.Analytics;
+using Microsoft.Azure.Mobile.Crashes;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace Todo
+{
+	public class App : Application
+	{
+		static TodoItemDatabase database;
+
+        protected override void OnStart()
+        {
+            MobileCenter.Start("android=8ac9fe4f-093d-479f-ad5d-fa32430f4c74;" +
+                   "uwp={Your UWP App secret here};" +
+                   "ios=49623aa9-16c3-4394-a7a9-8a8f92c20e6d;",
+                   typeof(Analytics), typeof(Crashes));
+        }
+
+        public App()
+		{
+			Resources = new ResourceDictionary();
+			Resources.Add("primaryGreen", Color.FromHex("91CA47"));
+			Resources.Add("primaryDarkGreen", Color.FromHex("6FA22E"));
+
+			var nav = new NavigationPage(new TodoListPage());
+			nav.BarBackgroundColor = (Color)App.Current.Resources["primaryGreen"];
+			nav.BarTextColor = Color.White;
+
+			MainPage = nav;
+		}
+
+		public static TodoItemDatabase Database
+		{
+			get
+			{
+				if (database == null)
+				{
+					database = new TodoItemDatabase(DependencyService.Get<IFileHelper>().GetLocalFilePath("TodoSQLite.db3"));
+				}
+				return database;
+			}
+		}
+
+		public int ResumeAtTodoId { get; set; }
+
+		protected override void OnStart()
+		{
+			//Debug.WriteLine("OnStart");
+
+			//// always re-set when the app starts
+			//// users expect this (usually)
+			////			Properties ["ResumeAtTodoId"] = "";
+			//if (Properties.ContainsKey("ResumeAtTodoId"))
+			//{
+			//	var rati = Properties["ResumeAtTodoId"].ToString();
+			//	Debug.WriteLine("   rati=" + rati);
+			//	if (!String.IsNullOrEmpty(rati))
+			//	{
+			//		Debug.WriteLine("   rati=" + rati);
+			//		ResumeAtTodoId = int.Parse(rati);
+
+			//		if (ResumeAtTodoId >= 0)
+			//		{
+			//			var todoPage = new TodoItemPage();
+			//			todoPage.BindingContext = await Database.GetItemAsync(ResumeAtTodoId);
+			//			await MainPage.Navigation.PushAsync(todoPage, false); // no animation
+			//		}
+			//	}
+			//}
+		}
+
+		protected override void OnSleep()
+		{
+			//Debug.WriteLine("OnSleep saving ResumeAtTodoId = " + ResumeAtTodoId);
+			//// the app should keep updating this value, to
+			//// keep the "state" in case of a sleep/resume
+			//Properties["ResumeAtTodoId"] = ResumeAtTodoId;
+		}
+
+		protected override void OnResume()
+		{
+			//Debug.WriteLine("OnResume");
+			//if (Properties.ContainsKey("ResumeAtTodoId"))
+			//{
+			//	var rati = Properties["ResumeAtTodoId"].ToString();
+			//	Debug.WriteLine("   rati=" + rati);
+			//	if (!String.IsNullOrEmpty(rati))
+			//	{
+			//		Debug.WriteLine("   rati=" + rati);
+			//		ResumeAtTodoId = int.Parse(rati);
+
+			//		if (ResumeAtTodoId >= 0)
+			//		{
+			//			var todoPage = new TodoItemPage();
+			//			todoPage.BindingContext = await Database.GetItemAsync(ResumeAtTodoId);
+			//			await MainPage.Navigation.PushAsync(todoPage, false); // no animation
+			//		}
+			//	}
+			//}
+		}
+	}
+}
+
